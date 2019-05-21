ab8cbdcae664c1c25f54ea7bb678cd4182d77288
Lukas Kiefer <lukaskiefer@Lukass-Air.fritz.box>
2019-05-21 22:43:53 +0200

    added handlebar templates for the notes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 function navigateToDetailsPage() {
   window.location.href = "note.html";
 }
+
+window.onload = function() {
+  const noteTemplate = document.getElementById("note_template").innerHTML;
+  const context = { notes: JSON.parse(localStorage.getItem("notes")) };
+  const compiledTemplate = Handlebars.compile(noteTemplate);
+  const html = compiledTemplate(context);
+  document.getElementById("notes_wrapper").innerHTML = html;
+};
